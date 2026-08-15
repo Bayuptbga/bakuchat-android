e681e9fcb6349d738765623f9dea65dfae535e86
Bayuptbga <114854983+Bayuptbga@users.noreply.github.com>
2026-08-15 19:03:08 +0000

    extract source.zip

---
+++ app/src/main/java/com/bakuchat/id/DelegationService.java
@@ -0,0 +1,14 @@
+package com.bakuchat.id;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
